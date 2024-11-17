4b63f10dd90659d9dfcd1e999586ae049aee9eb9
aeonBTC <7865680+aeonBTC@users.noreply.github.com>
2024-11-16 19:09:38 -0800

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,6 +17,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://raw.githubusercontent.com/guardianproject/gpmaven/master") }
     }
 }
 
